c17bb7dca6c1ba2dafdbfb7f2ffd403ee3905f6b
luischaparoc <lfchaparro@outlook.com>
2019-06-19 17:45:33 +0000

    Updating 4-puts.c file. Changing sintaxis

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts (str);
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }

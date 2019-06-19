61ef3248979dc7e8de7187e37c86e86a22a6eb81
luischaparoc <lfchaparro@outlook.com>
2019-06-19 18:11:22 +0000

    Adding 5-printf.c file. It prints a message with printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of artq");
+	return (0);
+}

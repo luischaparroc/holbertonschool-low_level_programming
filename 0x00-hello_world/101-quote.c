b1deb02e6b7701a15a19eb30235227ee6993ce7b
luischaparoc <lfchaparro@outlook.com>
2019-06-19 20:49:28 +0000

    Adding 101-quote.c file. It shows a message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,70 +1,15 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	putchar('"');
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	putchar('\n');
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, str1, 59);
 	return (1);
 }

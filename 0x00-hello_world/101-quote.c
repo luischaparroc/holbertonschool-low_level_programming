f02e2d10510542b8191a155ab6bd5d8e6f81fdec
luischaparoc <lfchaparro@outlook.com>
2019-06-19 19:38:15 +0000

    Adding 101-quote file. It prints a message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('"');
+	putchar('a');
+	putchar('n');
+	putchar('d');
+	putchar(' ');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('p');
+	putchar('i');
+	putchar('e');
+	putchar('c');
+	putchar('e');
+	putchar(' ');
+	putchar('o');
+	putchar('f');
+	putchar(' ');
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(' ');
+	putchar('i');
+	putchar('s');
+	putchar(' ');
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+	putchar('l');
+	putchar('"');
+	putchar(' ');
+	putchar('-');
+	putchar(' ');
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(' ');
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(' ');
+	putchar('2');
+	putchar('0');
+	putchar('1');
+	putchar('5');
+	putchar('-');
+	putchar('1');
+	putchar('0');
+	putchar('-');
+	putchar('1');
+	putchar('9');
+	putchar('\n');
+	return (1);
+}

9e01f63d497eaa6afa3afd3d36be7cd2f4989b73
luischaparoc <lfchaparro@outlook.com>
2019-06-19 17:41:26 +0000

    Updating 4-puts.c file to Holberton's Betty Coding Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-  char str [] = "\"Programming is like building a multilingual puzzle";
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+  char str[] = "\"Programming is like building a multilingual puzzle";
   puts (str);
-  return 0;
+  return (0);
 }

607003b234ecc1721aaccc214daeb2e989b911e8
luischaparoc <lfchaparro@outlook.com>
2019-06-20 20:44:44 +0000

    Fixing file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,46 +7,42 @@
  */
 int main(void)
 {
-	int i, e, g, h;
+int i, e, g, h, op1, op2;
 
-	i = 48;
-	e = 48;
+i = e = g = h = 48; 
+while (h < 58)
+{
 	g = 48;
-	h = 48;
-
-	while (h < 58)
+	while (g < 58)
 	{
-		g = 48;
-		while (g < 58)
+		e = 48;
+		while (e < 58)
 		{
-			e = 48;
-			while (e < 58)
+			i = 48;
+			while (i < 58)
 			{
-				i = 48;
-				while (i < 58)
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
 				{
-					if ((h + g) < (e + i))
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
+				i++;
 			}
-			g++;
+			e++;
 		}
-		h++;
+		g++;
 	}
-	putchar('\n');
-	return (0);
+	h++;
+}
+putchar('\n');
+return (0);
 }

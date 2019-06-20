fafb629287ac6bec51c31c230683cd3ffb544257
luischaparoc <lfchaparro@outlook.com>
2019-06-20 19:10:41 +0000

    Adding 100-print_comb3.c file. It works!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99 without prints the same combination of numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}

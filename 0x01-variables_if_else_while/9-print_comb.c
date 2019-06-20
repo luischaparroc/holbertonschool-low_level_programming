f9ecae6d0e2ce05457a19967f87a3cb1f5fbd2f8
luischaparoc <lfchaparro@outlook.com>
2019-06-20 17:46:07 +0000

    Fixing 9-print_comb.c file. It shows commas and spaces in the correct way

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Prints numbers between 0 to 9 with commas and spaces.
  *
  * Return: Always 0 (Success)
  */
@@ -14,8 +14,8 @@ int main(void)
 		putchar(i);
 		if (i != 57)
 		{
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');

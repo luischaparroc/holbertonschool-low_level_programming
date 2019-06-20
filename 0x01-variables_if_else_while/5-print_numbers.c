ae2daf36d6e25dea5fffe46665e5c05542d6c977
luischaparoc <lfchaparro@outlook.com>
2019-06-20 17:18:15 +0000

    Adding 5-print_numbers.c file. It works!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}

07bfef4a4bfc18063a05b718510452a3ed80560d
luischaparoc <lfchaparro@outlook.com>
2019-06-20 17:40:59 +0000

    9-print_comb.c file. It works!

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

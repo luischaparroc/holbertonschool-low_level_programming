35f44bb478f62d308e13ee050bd6f61bd7314b41
luischaparoc <lfchaparro@outlook.com>
2019-06-20 17:26:46 +0000

    Adding 6-print_numberz.c file. It works!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

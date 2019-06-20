cccec51755360caeebfdad9ca189528552f77a7c
luischaparoc <lfchaparro@outlook.com>
2019-06-20 18:13:13 +0000

    Adding 10-print_comb2.c file. It works!

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(e);
+			putchar(i);
+			if (i != 57 && e != 58)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}

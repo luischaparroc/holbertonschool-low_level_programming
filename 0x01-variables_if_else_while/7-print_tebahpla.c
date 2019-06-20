f60b8486f03ee18286b82cbc8a4540766d4af8ea
luischaparoc <lfchaparro@outlook.com>
2019-06-20 17:30:13 +0000

    Adding 7-print_tebahpla.c file. It works!

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

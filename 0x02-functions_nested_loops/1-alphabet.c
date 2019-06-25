f05cbc862c5dff0299f19c54c18f7309c7ebadd2
luischaparoc <lfchaparro@outlook.com>
2019-06-25 04:38:46 +0000

    Adding 1-alphabet.c file. It works!

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

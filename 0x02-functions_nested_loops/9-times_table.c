c2fea06a74268d78e5ebc6ccef2317a126aa0c48
luischaparoc <lfchaparro@outlook.com>
2019-06-25 04:43:12 +0000

    Adding 9-times_table.c file. It works!

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 0; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}

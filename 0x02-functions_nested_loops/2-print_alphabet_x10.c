5d63613b85f0614bea343a06e8da7c6d8736b761
luischaparoc <lfchaparro@outlook.com>
2019-06-25 04:39:40 +0000

    Adding 2-print_alphabet_x10.c file. It works!

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for(a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
+}

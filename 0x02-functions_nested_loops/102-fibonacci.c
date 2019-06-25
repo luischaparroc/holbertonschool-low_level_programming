5c94e107ca0e1636827115a21665ebb7a81dd6f2
luischaparoc <lfchaparro@outlook.com>
2019-06-25 04:44:55 +0000

    Adding 102-fibonacci.c file. It works!

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}

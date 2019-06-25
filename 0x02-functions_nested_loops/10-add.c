fa43b6ea14d807a550c24572f1e60a474a05028a
luischaparoc <lfchaparro@outlook.com>
2019-06-25 04:43:36 +0000

    Adding 10-add.c file. It works!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

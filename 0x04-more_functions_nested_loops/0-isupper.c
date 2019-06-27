da59904f6dc9d46a9fe596e8ac9e061c56232cef
luischaparoc <lfchaparro@outlook.com>
2019-06-27 14:22:48 +0000

    Adding 0-isupper.c file. It works!

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

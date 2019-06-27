85b4e9a6ee88e11616c9c47ce7c7166f830e50ae
luischaparoc <lfchaparro@outlook.com>
2019-06-27 15:02:20 +0000

    Adding 5-more_numbers.c file. It works!

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch < 10)
+				_putchar(ch + 48);
+			else
+			{
+				_putchar((ch / 10) + 48);
+				_putchar((ch % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}

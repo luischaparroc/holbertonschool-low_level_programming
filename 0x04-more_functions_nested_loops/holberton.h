cc04f55e47125bb5d2b7eb69d6bdad24bd5c2a27
luischaparoc <lfchaparro@outlook.com>
2019-06-27 16:43:10 +0000

    Adding holberton header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif

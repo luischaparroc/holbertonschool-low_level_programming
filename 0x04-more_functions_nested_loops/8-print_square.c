7d3cd7794a633adea31375b3e85bd62d989507e3
luischaparoc <lfchaparro@outlook.com>
2019-06-27 15:50:32 +0000

    Adding 8-print_square.c file. It works!

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}

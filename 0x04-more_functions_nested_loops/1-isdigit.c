8625f88080911fbd5a7860267705da7014840b8f
luischaparoc <lfchaparro@outlook.com>
2019-06-27 14:27:44 +0000

    Adding 1-isdigit.c file. It works!

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

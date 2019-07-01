3da110be95d05bd02c4cbd73379d007c229f3e12
luischaparoc <lfchaparro@outlook.com>
2019-07-01 17:36:57 +0000

    Adding 5-rev_string.c file. It works!

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,15 +12,11 @@ void print_rev(char *s)
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
-		{
 			break;
-		}
 		count++;
 	}
 
 	for (count--; count >= 0; count--)
-	{
 		_putchar(s[count]);
-	}
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}

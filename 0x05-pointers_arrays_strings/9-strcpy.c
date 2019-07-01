3cebc3d3cfcd26724c3c5fcf6d8801ea0b02243a
luischaparoc <lfchaparro@outlook.com>
2019-07-01 20:13:59 +0000

    Adding 9-strcpy.c file. It works!

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (src[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	return(dest);
+}

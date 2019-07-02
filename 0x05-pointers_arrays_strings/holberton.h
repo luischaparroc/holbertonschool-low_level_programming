7b969c6569ff672c7524dee8abf04987c21b8334
luischaparoc <lfchaparro@outlook.com>
2019-07-02 05:24:50 +0000

    Adding header file

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif

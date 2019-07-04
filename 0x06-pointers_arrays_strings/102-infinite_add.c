bf97321c7b4e7e1110571f8b943e6a05316d27af
luischaparoc <lfchaparro@outlook.com>
2019-07-04 23:28:24 +0000

    Adding 102-infinite_add.c file. It works!

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -12,5 +12,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
  
 #endif

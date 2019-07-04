3e3f6d24663c1b826b1ec8f039f37d89e2774adb
luischaparoc <lfchaparro@outlook.com>
2019-07-04 17:14:50 +0000

    Adding 7-leet.c file. It works!

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}

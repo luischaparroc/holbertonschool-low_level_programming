a81c67b20acc897410bb92c17335cfaa90bfbd58
luischaparoc <lfchaparro@outlook.com>
2019-07-04 19:30:38 +0000

    Adding 100-print_number.c file. It works!

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,5 +10,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
  
 #endif

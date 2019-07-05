1d8c195bcab2c86b1077d4d8c0a195f8d5bfed8e
luischaparoc <lfchaparro@outlook.com>
2019-07-05 04:37:37 +0000

    Fixing errors

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,29 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
  * print_buffer - prints a buffer
  * @b: buffer.
  * @size: size of buffer.
  * Return: no return.
  */
-
 void print_buffer(char *b, int size)
 {
-	int i, j, k, l;
-	char printable[size];
+	int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
 	else
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (*(b + i) < 32 || *(b + i) > 126)
-				printable[i] = '.';
-			else
-				printable[i] = *(b + i);
-		}
-
 		for (j = 0; j < size; j += 10)
 		{
 			printf("%08x", j);
@@ -38,7 +29,10 @@ void print_buffer(char *b, int size)
 			}
 			printf(" ");
 			for (l = j; l < j + 10 && l < size; l++)
-				printf("%c", printable[l]);
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
 			printf("\n");
 		}
 	}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,5 +14,5 @@ void print_number(int n);
 int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
- 
+
 #endif

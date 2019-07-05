f75cfc643bd5d235c72d6dc5d0fcf0755bb31365
luischaparoc <lfchaparro@outlook.com>
2019-07-05 04:50:53 +0000

    Fixing errors 2

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -28,11 +28,15 @@ void print_buffer(char *b, int size)
 					printf("  ");
 			}
 			printf(" ");
-			for (l = j; l < j + 10 && l < size; l++)
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
 				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
 				else
 					printf("%c", *(b + l));
+			}
 			printf("\n");
 		}
 	}

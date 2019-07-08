7e7b78910cd81221ddba0e94d5b8421eea26de79
luischaparoc <lfchaparro@outlook.com>
2019-07-08 17:32:01 +0000

    Adding 2-strchr.c file. It works!

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0, j = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c && j == 0)
+		{
+			j = i;
+			break;
+		}
+	if (j == 0)
+		return (s + i);
+	return (s + j);
+}

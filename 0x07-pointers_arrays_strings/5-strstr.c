035a4cc662af785f7342f5b354470fe4eef049ef
luischaparoc <lfchaparro@outlook.com>
2019-07-08 23:52:45 +0000

    Fixing errors

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,26 +7,31 @@
  * @needle: substring.
  * Return: pointer to the beginning of located substring or
  * NULL if the substring is not found.
- */
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, lh, ln, bool;
 
-	for (i = 0; *(haystack + i) != '\0'; i++)
+	for (lh = 0; *(haystack + lh) != '\0'; lh++)
+		;
+	for (ln = 0; *(needle + ln) != '\0'; ln++)
+                ;
+	for (i = 0; i < lh; i++)
 	{
 		bool = 1;
-		for (j = 0; *(needle + j) != '\0'; j++)
+		for (j = 0; j < ln; j++)
 		{
 			if (*(haystack + j + i) == *(needle + j))
 				bool = 0;
 			else
 			{
 				bool = 1;
+				i += j;
 				break;
 			}
 		}
 		if (bool == 0)
 			return (haystack + i);
 	}
-	return ('\0');
+	return (0);
 }

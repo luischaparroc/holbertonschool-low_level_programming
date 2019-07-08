981badb6768a9c8fe18186a28d7b6e1df38131c6
luischaparoc <lfchaparro@outlook.com>
2019-07-08 18:51:09 +0000

    Updating file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,7 +7,7 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i = 0, m = 0;
+	unsigned int i, m = 0;
 
 	for (i = 0; i < 64; i++)
 	{
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif

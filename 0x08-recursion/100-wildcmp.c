feecffd835322f4145c96471c4325d5713360dd5
luischaparoc <lfchaparro@outlook.com>
2019-07-10 23:48:46 +0000

    Updating file (line 15)

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * wildcmp - compares two strings.
  * @s1: string 1.
@@ -12,9 +12,11 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
 	return (0);
 }

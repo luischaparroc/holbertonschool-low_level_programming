98b7047d8fc715d797b1b038e43b93c6a989256e
luischaparoc <lfchaparro@outlook.com>
2019-07-10 18:20:16 +0000

    It doesn't count negative numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_prime(int n, int c)
+int is_prime(unsigned int n, unsigned int c)
 {
 	if (n % c == 0)
 	{
@@ -27,7 +27,7 @@ int is_prime_number(int n)
 	if (n == 0)
 		return (0);
 	if (n < 0)
-		n *= -1;
+		return (0);
 	if (n == 1)
 		return (0);
 	return (is_prime(n, 2));

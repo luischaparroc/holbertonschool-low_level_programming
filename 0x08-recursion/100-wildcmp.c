cdcc214fdd14f23f5fcbd267df1fa2af7e2bcab8
luischaparoc <lfchaparro@outlook.com>
2019-07-10 22:18:34 +0000

    Uploading function. It works with any case

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) != '\0')
+		return (0);
+	return (0);
+}

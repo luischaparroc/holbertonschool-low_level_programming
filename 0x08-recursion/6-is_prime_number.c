69b73a3d012bf60833ab4e50e7889a86cba87344
luischaparoc <lfchaparro@outlook.com>
2019-07-10 17:47:06 +0000

    Adding prime_number function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime(int n, int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		n *= -1;
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,5 +8,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif

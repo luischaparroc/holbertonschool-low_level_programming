8f9b303f39a3f6d78fa09e6539d156b0831e49dc
luischaparoc <lfchaparro@outlook.com>
2019-07-10 22:20:32 +0000

    Fixing line 17

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 		return (1);
-	if (*s2 == '*' && *(s1 + 1) != '\0')
+	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
 		return (0);
 	return (0);
 }

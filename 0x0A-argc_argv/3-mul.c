4e7c4bd8c181137f4b36b7c0f479c5eac453b0c1
luischaparoc <lfchaparro@outlook.com>
2019-07-12 21:50:04 +0000

    Uploading program (task 3)

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}

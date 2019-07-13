2b73ccf9327ece8e2b6619779970d84bce99a61e
luischaparoc <lfchaparro@outlook.com>
2019-07-13 17:57:56 +0000

    Updating file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds positive numbers.
  * @argc: number of command line arguments.
@@ -12,15 +13,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		j = 0;
-		while (argv[i][j] != '\0')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			j++;
 		}
 		add += atoi(argv[i]);
 	}

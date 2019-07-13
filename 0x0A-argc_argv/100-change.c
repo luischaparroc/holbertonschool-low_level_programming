6ad60329db69ee61af512b4cb394907e7fdaaca7
luischaparoc <lfchaparro@outlook.com>
2019-07-13 11:36:30 +0000

    Deleting unnecessary conditional

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,22 +19,19 @@ int main(int argc, char *argv[])
 
 	cents = atoi(argv[1]);
 
-	if (cents > 0)
+	while (cents > 0)
 	{
-		while (cents > 0)
-		{
-			if (cents >= 25)
-				cents -= 25;
-			else if (cents >= 10)
-				cents -= 10;
-			else if (cents >= 5)
-				cents -= 5;
-			else if (cents >= 2)
-				cents -= 2;
-			else if (cents >= 1)
-				cents -= 1;
-			ncoins += 1;
-		}
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
 	printf("%d\n", ncoins);
 	return (0);

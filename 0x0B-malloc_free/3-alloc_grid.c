a87452604723fbecba1528243975519f819433cc
luischaparoc <lfchaparro@outlook.com>
2019-07-16 17:56:58 +0000

    Fixing error: freeing memory space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,6 +28,8 @@ int **alloc_grid(int width, int height)
 		gridout[i] = malloc(width * sizeof(int));
 		if (gridout[i] == NULL)
 		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
 			free(gridout);
 			return (NULL);
 		}

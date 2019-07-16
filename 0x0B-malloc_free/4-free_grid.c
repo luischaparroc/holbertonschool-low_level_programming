196f49d255a08d28d238be1dd10fb22a5c76bb1e
luischaparoc <lfchaparro@outlook.com>
2019-07-16 18:25:27 +0000

    Fixing error: including description of grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,16 +3,14 @@
 
 /**
  * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
  * @height: height of the grid.
  *
  * Return: no return
  */
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height > 0)
-	{
-		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
-	}
+	for (; height >= 0; height--)
+		free(grid[height]);
+	free(grid);
 }

b14daa171eb1a6be8e1ed99c03251517e9cee0fe
luischaparoc <lfchaparro@outlook.com>
2019-07-16 14:56:36 +0000

    Uploading a function that creates an array of char. It works

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size);
+	for (i = 0; i < size; i++)
+	{
+		cr[i] = c;
+	}
+	return (cr);
+}

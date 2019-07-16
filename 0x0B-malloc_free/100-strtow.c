1da322a17d04581b40da43ddeb045970a42b6c79
luischaparoc <lfchaparro@outlook.com>
2019-07-16 21:56:01 +0000

    Uploading function that splits a string into words

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
+char **strtow(char *str)
+{
+	char **aout;
+	int c, height, i, j, a1;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] == ' ' && str[c + 1] != ' ' && str[c + 1] != '\0')
+			height++;
+	aout = malloc(height * sizeof(char *));
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+	for (i = a1 = 0; i < height; i++, j++)
+	{
+		for (c = a1; str[c] != '\0'; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					for (i--; i >= 0; i--)
+						free(aout[i]);
+					free(aout);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		j = 0;
+		for (; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
+	}
+	aout[i] = NULL;
+	return (aout);
+}

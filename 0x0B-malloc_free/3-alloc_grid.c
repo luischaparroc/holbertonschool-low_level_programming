1074ae39c9bddfd5513d93155e244522fbe1b785
luischaparoc <lfchaparro@outlook.com>
2019-07-16 17:53:14 +0000

    Fixing error. Inused memory space free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,19 @@ int **alloc_grid(int width, int height)
 
 	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
+	{
+		free(gridout);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		gridout[i] = malloc(width * sizeof(int));
 		if (gridout[i] == NULL)
+		{
+			free(gridout);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)

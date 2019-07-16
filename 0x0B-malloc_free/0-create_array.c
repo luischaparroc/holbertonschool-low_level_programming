4e78a130ce04a01d3606663d563f842400f2c85e
luischaparoc <lfchaparro@outlook.com>
2019-07-16 15:23:30 +0000

    Fixing error. Returns NULL if insufficient memory is available

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
-	{
 		cr[i] = c;
-	}
+
 	return (cr);
 }

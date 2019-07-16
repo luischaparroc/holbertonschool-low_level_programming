b014723b33fd059ff1f7cd4a13c19c8bb947617d
luischaparoc <lfchaparro@outlook.com>
2019-07-16 18:17:40 +0000

    Uploading header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,5 +5,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif

f2c93ae02bbfaa9255449b3edced6fee88c64c76
luischaparoc <lfchaparro@outlook.com>
2019-07-16 15:28:24 +0000

    Uploading function that returns a pointer of a copy of a string given. It works

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned int i, j;
+
+	if (*str == '\0')
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strout = malloc(sizeof(char) * i);
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		strout[j] = str[j];
+
+	return (strout);
+}

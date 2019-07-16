a2e959c546c5bd03f552e5a51871ceafd531b934
luischaparoc <lfchaparro@outlook.com>
2019-07-16 19:24:57 +0000

    Uploading function that concatenates arguments

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of integers
+ */
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int c, i, j, ia;
+
+	if (ac == 1 || av == NULL)
+		return (NULL);
+
+	for (c = i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+
+	aout = malloc(c * sizeof(char));
+
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+
+	return (aout);
+}

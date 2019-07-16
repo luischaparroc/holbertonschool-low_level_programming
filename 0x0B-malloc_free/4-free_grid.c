f087fd9414d4e6726beeac550d58cd54eb69fadb
luischaparoc <lfchaparro@outlook.com>
2019-07-16 18:24:08 +0000

    Fixing error: including exceptional cases

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,7 +9,10 @@
  */
 void free_grid(int **grid, int height)
 {
-	for (height--; height >= 0; height--)
-		free(grid[height]);
-	free(grid);
+	if (grid != NULL && height > 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }

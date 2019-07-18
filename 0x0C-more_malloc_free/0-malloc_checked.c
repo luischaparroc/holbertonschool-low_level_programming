c7c4531facdb8064533ac8007b0189e3d44f2aed
luischaparoc <lfchaparro@outlook.com>
2019-07-18 17:02:54 +0000

    Uploading function that allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory.
+ * @b: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}

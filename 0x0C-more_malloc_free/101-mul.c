3c281749c8bfd07231085055e6d16b48b97277ac
luischaparoc <lfchaparro@outlook.com>
2019-07-19 02:38:00 +0000

    Updating function. It works

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,22 +3,22 @@
 #include <stdio.h>
 
 /**
- * _comp_ln - determines bigger number for malloc.
- * @n1: first number.
- * @n2: second number.
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
  *
- * Return: bigger number for malloc.
+ * Return: pointer of a char array.
  */
-int _comp_ln(int n1, int n2)
+char *_initialize_array(char *ar, int lar)
 {
-	int nout;
+	int i = 0;
 
-	if (n1 > n2)
-		nout = n1 + 1;
-	else
-		nout = n2 + 1;
-	return (nout);
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
+
 /**
  * _checknum - determines length of the number
  * and checks if number is in base 10.
@@ -51,42 +51,42 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	ln1 = _checknum(argv, 1);
-	ln2 = _checknum(argv, 2);
-	lnout = _comp_ln(ln1, ln2);
-	nout = malloc(lnout + 1);
-	nout[lnout] = '\0';
-	for (k = lnout - 1, i = ln1 - 1, j = ln2 - 1, addl = 0; k >= 0; k--, i--, j--)
-	{
-		if (k == 0 && addl == 0 && i < 0 && j < 0)
+		if (i < 0)
 		{
-			free(nout);
-			k = lnout - 1;
-			nout = malloc(k + 1);
-			nout[k] = '\0';
-			i = ln1 - 1;
-			j = ln2 - 1;
-			k--;
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
-		if (i >= 0 && j >= 0)
-			add = (argv[1][i] - '0') + (argv[2][j] - '0') + addl;
-		else if (i >= 0)
-			add = (argv[1][i] - '0') + addl;
-		else if (j >= 0)
-			add = (argv[2][j] - '0') + addl;
-		else
-			add = addl;
-		addl = add / 10;
-		add = add % 10;
-		nout[k] = add + '0';
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1);
+			nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
 	printf("%s\n", nout);
 	return (0);

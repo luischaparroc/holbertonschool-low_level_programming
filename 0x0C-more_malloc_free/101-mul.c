98a4dac3efdc32e5ed73b1a346a83ec55467cb9d
luischaparoc <lfchaparro@outlook.com>
2019-07-19 03:17:34 +0000

    Fixing: works with 0 values

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
+/**
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
+ */
+void _is_zero(char *argv[] __attribute__((unused)))
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
+
 /**
  * _initialize_array - set memery to zero in a new array
  * @ar: char array.
@@ -57,7 +89,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);

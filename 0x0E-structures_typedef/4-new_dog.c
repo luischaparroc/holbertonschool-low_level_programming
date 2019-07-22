10023c7ca4a147a218e44088ff562d0ee72f5b2e
luischaparoc <lfchaparro@outlook.com>
2019-07-22 16:20:00 +0000

    Fixing. Applying dynamic allocation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
+	int i, lname, lowner;
 
 	p_dog = malloc(sizeof(*p_dog));
 	if (p_dog == NULL)
 		return (NULL);
 
-	p_dog->name = name;
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	p_dog->name = malloc(lname + 1);
+	if (!(p_dog->name))
+		return (NULL);
+
+	p_dog->owner = malloc(lowner + 1);
+	if (!(p_dog->owner))
+		return (NULL);
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
 	p_dog->age = age;
-	p_dog->owner = owner;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
 
 	return (p_dog);
 }

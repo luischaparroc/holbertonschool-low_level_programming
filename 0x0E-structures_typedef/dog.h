d4f9b72dec02f49a0ceab4a8f8707cd7ae1c070f
luischaparoc <lfchaparro@outlook.com>
2019-07-22 14:45:27 +0000

    Updating header file. It contains a typedef named dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
  * @age: age of the dog

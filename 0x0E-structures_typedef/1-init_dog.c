be3cbe9cef9c55e858a98ec21fa98b6735723ed1
luischaparoc <lfchaparro@outlook.com>
2019-07-22 17:22:47 +0000

    Fixing: adding NULL case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }

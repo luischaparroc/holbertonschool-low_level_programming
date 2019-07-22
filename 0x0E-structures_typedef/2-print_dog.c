037e16a0ab3fc2d7c1d140d1bcd177c52c157f44
luischaparoc <lfchaparro@outlook.com>
2019-07-22 14:35:17 +0000

    Uploading function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif

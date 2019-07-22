79b53a72a1f8a44d49fce49958851c26cfc21bbc
luischaparoc <lfchaparro@outlook.com>
2019-07-22 17:27:37 +0000

    Fixing: function prints when age is 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,18 +16,11 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!(d->age))
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("\n");
 	}
 }

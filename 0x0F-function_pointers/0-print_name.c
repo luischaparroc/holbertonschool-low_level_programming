c6c2df02fbc4e9a0ee3d89a5739711dcaef32857
luischaparoc <lfchaparro@outlook.com>
2019-07-24 14:47:31 +0000

    Uploading function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif

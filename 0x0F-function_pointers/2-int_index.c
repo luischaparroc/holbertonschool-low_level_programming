4a58d052a60a6b9a49fdff9eb0d687f4d87f7633
luischaparoc <lfchaparro@outlook.com>
2019-07-24 17:16:47 +0000

    Fixing: it doesn't count NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
 
 	return (-1);
 }

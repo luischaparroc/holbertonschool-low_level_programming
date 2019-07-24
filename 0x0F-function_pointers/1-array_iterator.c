1ec3bb43e3e94364a79a07e9001763cc33dda3b7
luischaparoc <lfchaparro@outlook.com>
2019-07-24 17:10:13 +0000

    Fixing: it doesn't count NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }

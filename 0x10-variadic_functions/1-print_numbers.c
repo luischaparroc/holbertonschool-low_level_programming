b8d185fdb154a765710d777771dc16aeb3ed1643
luischaparoc <lfchaparro@outlook.com>
2019-07-26 14:11:25 +0000

    [Update] Uploading function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
+ * Return: no return.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (separator)
+	{
+		if (n)
+		{
+			va_start(valist, n);
+
+			for (i = 0; i < n - 1; i++)
+				printf("%d%s", va_arg(valist, int), separator);
+
+			printf("%d\n", va_arg(valist, int));
+			va_end(valist);
+		}
+	}
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif

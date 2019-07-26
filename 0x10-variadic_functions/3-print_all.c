ef6bcbf412c5a0e093e66c47489e6c56b378390d
luischaparoc <lfchaparro@outlook.com>
2019-07-26 19:09:36 +0000

    Feature: uploading function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c != 0)
+			{
+				printf(", ");
+				break;
+			}, j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c++;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c++;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c++;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c++;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		}, i++;
+	}
+	printf("\n"), va_end(valist);
+}

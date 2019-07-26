a14272aeded1eb056afe951706b83f1f3b862aaa
luischaparoc <lfchaparro@outlook.com>
2019-07-26 16:49:57 +0000

    Fix: changing string for NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (str)
 				printf("%s", str);
 			else
-				printf("(nill)");
+				printf("(nil)");
 
 			if (i == n - 1)
 				printf("\n");

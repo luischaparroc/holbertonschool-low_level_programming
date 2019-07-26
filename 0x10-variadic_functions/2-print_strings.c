7efce2847c6414fb4053ecde89020199cbfecd05
luischaparoc <lfchaparro@outlook.com>
2019-07-26 16:48:44 +0000

    Feature: uploading function that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings.
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ *
+ * Return: no return.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *str;
+
+	if (separator && n)
+	{
+		va_start(valist, n);
+
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(valist, char *);
+
+			if (str)
+				printf("%s", str);
+			else
+				printf("(nill)");
+
+			if (i == n - 1)
+				printf("\n");
+			else
+				printf("%s", separator);
+		}
+
+		va_end(valist);
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,6 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif

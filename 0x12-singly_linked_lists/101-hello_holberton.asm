fbe95d4a094dd2ad300cd9cd5cb0c05c2df94210
luischaparoc <lfchaparro@outlook.com>
2019-08-01 21:12:05 +0000

    [Feature]: uploading assembly code that prints a message Created: 101-hello_holberton.asm file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+        SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+        mov ebx, 0
+        mov eax, 1
+        int 0x80

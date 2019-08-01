8fa38703c1894c1b8b5f59e31ad73eb1b1d2ad40
luischaparoc <lfchaparro@outlook.com>
2019-08-01 17:06:43 +0000

    [Feature]: uploading function that adds a new node at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t nchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		new->str = strdup("(nil)");
+	else
+		new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}

bb8d9e13929b1cbedffcdd2d286774efdc83abc0
luischaparoc <lfchaparro@outlook.com>
2019-08-01 19:29:50 +0000

    [Feature]: uploading function that is executed before main()

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}

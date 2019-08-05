8dedaa5237d3bd7a82a1063860a9adb8835008e6
luischaparoc <lfchaparro@outlook.com>
2019-08-05 20:28:16 +0000

    [Feature]: uploading funcion that deletes the head node of a list Created: 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	int hnode;
+	listint_t *h;
+	listint_t *curr;
+
+	if (*head == NULL)
+		return (0);
+
+	curr = *head;
+
+	hnode = curr->n;
+
+	h = curr->next;
+
+	free(curr);
+
+	*head = h;
+
+	return (hnode);
+}

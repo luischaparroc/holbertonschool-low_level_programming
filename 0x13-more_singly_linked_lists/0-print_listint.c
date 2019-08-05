6266c593033c082a03fd0ebc556514f53cba560a
luischaparoc <lfchaparro@outlook.com>
2019-08-05 20:21:58 +0000

    [Feature]: uploading function that prints all the elements of a linked list Created: lists.h and 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}

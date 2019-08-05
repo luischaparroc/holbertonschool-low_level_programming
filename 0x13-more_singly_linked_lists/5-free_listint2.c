dbbfbdc367cd674b3a5c8066f56f07a0d053d0cf
luischaparoc <lfchaparro@outlook.com>
2019-08-05 20:27:26 +0000

    [Feature]: uploading function that frees a linked list. Created: 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	curr = *head;
+	while ((temp = curr) != NULL)
+	{
+		curr = curr->next;
+		free(temp);
+	}
+	*head = NULL;
+}

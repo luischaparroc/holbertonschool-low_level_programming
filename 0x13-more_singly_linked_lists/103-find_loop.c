32bc285dad6483a2a1fe4965357f95e06e237147
luischaparoc <lfchaparro@outlook.com>
2019-08-06 20:02:23 +0000

    [Fix]: it works with all cases. Modified: 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,32 +8,35 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *bigger;
-	int control;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (head == NULL)
-		return (NULL);
-
-	bigger = NULL;
-	control = 0;
-
-	while (head->next != NULL)
+	p2 = head;
+	prev = head;
+	while (head && p2 && p2->next)
 	{
-		if (bigger == head)
-		{
-			control++;
-			if (control == 3)
-				break;
-		}
-
-		if ((long int)head->next > (long int)head && control == 0)
-		{
-			bigger = head->next->next;
-			control = 1;
-		}
 		head = head->next;
+		p2 = p2->next->next;
+
+		if (head == p2)
+		{
+			head = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != head && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == head)
+					break;
+
+				head = head->next;
+			}
+			return (p2->next);
+		}
 	}
-	if (head->next == NULL)
-		return (NULL);
-	return (bigger);
+
+	return (NULL);
 }

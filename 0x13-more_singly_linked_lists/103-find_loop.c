ed329b5839995c8f88a7ab5356f819015114bad2
luischaparoc <lfchaparro@outlook.com>
2019-08-06 15:55:34 +0000

    [Fix] = it works with any case Modified: 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,15 +9,27 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *curr;
+	listint_t *find_loop;
+
+	curr = head;
+	find_loop = NULL;
 
 	while (head != NULL)
 	{
 		curr = head;
+
+		if (find_loop == curr)
+		{
+			return (curr);
+		}
+
 		head = head->next;
+
 		if ((long int)head > (long int)curr)
 		{
-			return (head);
+			find_loop = head;
 		}
+
 		curr = curr->next;
 	}
 

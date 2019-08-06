86b264592aa406cfd18ba1b56b9b2c62034ec3e0
luischaparoc <lfchaparro@outlook.com>
2019-08-06 13:51:30 +0000

    [Fix]: if list is NULL, it doesn't add a new node Modified: 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *h;
 
+	if (head == NULL)
+		return (NULL);
+
 	h = *head;
 
 	if (idx != 0)

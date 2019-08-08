372e337a96f87b4084c4e7b6af0333f098d8afca
luischaparoc <lfchaparro@outlook.com>
2019-08-08 17:02:47 +0000

    [Feature]: uploading function that converts a binary number to an int Created: 0-binary_to_uint.c and holberton.h file.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <limits.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+
+#endif

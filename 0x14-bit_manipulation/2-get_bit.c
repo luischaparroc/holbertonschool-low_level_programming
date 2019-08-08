815e4e7e0284ba31046022a94079a6f0bcd7b2c7
luischaparoc <lfchaparro@outlook.com>
2019-08-08 17:28:32 +0000

    [Fix]: it works with n = 0 Modified: 2-get_bit.c file.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (n == 0 && index == 0)
+		return (0);
+
 	for (i = 0; i <= index && n != 0; n >>= 1, i++)
 	{
 		if (index == i)

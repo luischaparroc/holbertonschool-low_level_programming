8152027c399f07ef1099cd5017d0b4df4d763e0c
luischaparoc <lfchaparro@outlook.com>
2019-08-08 20:01:20 +0000

    [Feature]: uploading function that checks the endianness Created: 100-get_endianness.c file. Modified: holberton.h file.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}

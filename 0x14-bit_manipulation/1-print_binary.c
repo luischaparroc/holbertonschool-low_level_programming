34084d9a269dcec959572dbd4201c03b103c89ce
luischaparoc <lfchaparro@outlook.com>
2019-08-08 18:12:26 +0000

    [Update]: done with recursion Modified: 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <limits.h>
 
 /**
  * print_binary - prints the binary representation
@@ -10,30 +9,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long long int i;
-	int first_one;
-
-	if (n == 0)
+	if (n >> 0)
 	{
-		_putchar('0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
-		i = (unsigned long long)LONG_MAX + 1;
-		first_one = 0;
-
-		for (; i != 0; i >>= 1)
-		{
-			if (n & i)
-			{
-				_putchar('1');
-				first_one = 1;
-			}
-			else
-			{
-				if (first_one == 1)
-					_putchar('0');
-			}
-		}
+		_putchar('0');
 	}
 }

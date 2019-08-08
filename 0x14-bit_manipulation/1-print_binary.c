f22e9aabef19b78b06047141e46096544fe1f7ae
luischaparoc <lfchaparro@outlook.com>
2019-08-08 17:04:42 +0000

    [Feature]: uploading function that prints an int as a binary Created: 1-print_binary.c file.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long long int i;
+	int first_one;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	i = (unsigned long long)LONG_MAX + 1;
+	first_one = 0;
+
+	for (; i != 0; i >>= 1)
+	{
+		if (n & i)
+		{
+			_putchar('1');
+			first_one = 1;
+		}
+		else
+		{
+			if (first_one == 1)
+				_putchar('0');
+		}
+	}
+}

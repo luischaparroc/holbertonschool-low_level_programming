bdd1ee7bbc7c80f135a78d09b06cc432dfbd9cf1
luischaparoc <lfchaparro@outlook.com>
2019-08-08 17:55:22 +0000

    [Fix]: it works with index up to 63 Modified: 2-get_bit.c file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	for (i = 0; i <= index && n != 0; n >>= 1, i++)
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
 		if (index == i)
 		{

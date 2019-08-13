4bb51a0ecc1d8ea209d52072bbe37a560ade500f
luischaparoc <lfchaparro@outlook.com>
2019-08-13 17:50:21 +0000

    [Fix]: it truncates when the file exists Modified: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
-		text_content = "\n";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;

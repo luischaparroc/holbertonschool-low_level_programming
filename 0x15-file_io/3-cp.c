ac5a2ef181b1c2aaa84a7373f07bac0d135243a1
luischaparoc <lfchaparro@outlook.com>
2019-08-13 19:51:37 +0000

    [Feature]: uploading function that copies information Created: 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		write(file_to, buf, nchars);
+	}
+
+	err_close = close(file_from);
+	if (err_close)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", err_close);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", err_close);
+		exit(100);
+	}
+	return (0);
+}
+

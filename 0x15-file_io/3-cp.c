444d9da01793ef3d1d12ea71ef7f0cae428a14a4
luischaparoc <lfchaparro@outlook.com>
2019-08-13 20:03:54 +0000

    [Fix]: exit 100 when it is an error in closing files Modified: 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,16 +52,16 @@ int main(int argc, char *argv[])
 	}
 
 	err_close = close(file_from);
-	if (err_close)
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", err_close);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
 		exit(100);
 	}
 
 	err_close = close(file_to);
-	if (err_close)
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", err_close);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
 		exit(100);
 	}
 	return (0);

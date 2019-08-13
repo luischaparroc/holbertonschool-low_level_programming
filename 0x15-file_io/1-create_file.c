a06166b2f7efe33d4c741a61087af661d66723a3
luischaparoc <lfchaparro@outlook.com>
2019-08-13 17:45:10 +0000

    [Fix]: removing 1 Modified: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, text_content, nletters + 1);
+	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);

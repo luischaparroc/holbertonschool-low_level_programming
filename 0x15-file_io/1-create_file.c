b180bf35b9df065ccf6aa3ef1be5926f7083c9ed
luischaparoc <lfchaparro@outlook.com>
2019-08-13 17:41:55 +0000

    [Fix]: function truncates if file exists Modified: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	rwr = write(fd, text_content, nletters + 1);
 
 	if (rwr == -1)
 		return (-1);

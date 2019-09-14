446786386c91b68efed6061190d1d95e85cee968
luischaparoc <lfchaparro@outlook.com>
2019-09-14 20:21:50 +0000

    [Fix]: adding previous pointer to the new node Modified: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					{
 						new->n = n;
 						new->next = head->next;
+						new->prev = head;
 						head->next->prev = new;
 						head->next = new;
 					}

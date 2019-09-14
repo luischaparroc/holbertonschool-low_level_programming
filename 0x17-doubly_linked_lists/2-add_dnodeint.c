af5be2a8dc66ed8d9090b1513d7a053af2e6e92e
luischaparoc <lfchaparro@outlook.com>
2019-09-14 13:07:09 +0000

    [Feature]: adding a function that adds a new node at the beginning of a dll Created: 2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+
+	if (*head != NULL)
+	{
+		while ((*head)->prev != NULL)
+			*head = (*head)->prev;
+	}
+
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}

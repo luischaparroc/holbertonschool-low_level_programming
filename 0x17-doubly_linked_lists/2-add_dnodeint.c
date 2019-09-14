2b217dce5ef596bd91a46a10d994913fd58f40d0
luischaparoc <lfchaparro@outlook.com>
2019-09-14 19:52:41 +0000

    [Fix]: adding previous node to the last head Modified: 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	h = *head;
 
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
 	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
 
 	*head = new;
 

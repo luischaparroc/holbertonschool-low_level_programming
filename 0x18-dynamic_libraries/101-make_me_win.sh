d6d99469d4013e160bf845a6c0cef5c00bde96f3
luischaparoc <lfchaparro@outlook.com>
2019-09-16 21:54:12 +0000

    [Fix]: download dynamic library correctly Modified: 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/luischaparroc/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/random.so?raw=true
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/random.so
 export LD_PRELOAD=/tmp/random.so

8fcf6dea3abda0c8fba2fdf52456886b5a5eda28
luischaparoc <lfchaparro@outlook.com>
2019-09-17 12:51:37 +0000

    [Fix]: changing the name of the library Modified: 101-make_me_win.sh Created: nrandom.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/random.so
-export LD_PRELOAD=/tmp/random.so
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so

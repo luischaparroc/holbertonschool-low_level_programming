81a8ca5b8b1302e05d62c721035de710cac3504a
luischaparoc <lfchaparro@outlook.com>
2019-10-11 01:19:06 +0000

    [Feature]: adding a hash function that implements the djb1 algorithm Created: 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function that generates a random number through
+ * a string given (hash number)
+ *
+ * @str: string
+ * Return: hash number
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}

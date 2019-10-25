e20da5201d410fdb16ec778df39606a46296bc4c
luischaparoc <lfchaparro@outlook.com>
2019-10-25 23:14:15 +0000

    [Feature]: adding a function that executes the insertion-sort alg Big O notation for the following cases: 	-> O(n) for the best case 	-> O(n^2) for the average case 	-> O(n^2) for the worst case
    New file: 1-O, 1-insertion_sort_list.c and sort.h

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
+ *
+ * @list: doubly linked list
+ * Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *tmp;
+
+	ptr = *list;
+	while (ptr != NULL)
+	{
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			tmp = ptr->next;
+			ptr->next = tmp->next;
+			tmp->prev = ptr->prev;
+
+			if (ptr->prev)
+				ptr->prev->next = tmp;
+
+			if (tmp->next)
+				tmp->next->prev = ptr;
+
+			ptr->prev = tmp;
+			tmp->next = ptr;
+
+			if (tmp->prev)
+				ptr = tmp->prev;
+			else
+				*list = tmp;
+
+			print_list(*list);
+		}
+		ptr = ptr->next;
+	}
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -20,5 +20,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif

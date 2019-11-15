e67914d35e6a08a51405e6eb3fd3cb74a8d91e63
luischaparoc <lfchaparro@outlook.com>
2019-11-15 21:46:30 +0000

    [Feature]: adding a function that returns the perimeter of a grid New file: 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module that contains the island_perimeter function """
+
+
+def island_perimeter(grid):
+    """ Function that returns the perimeter of island described in grid """
+    perimeter = 0
+
+    nrows = len(grid)
+    ncolumns = len(grid)
+
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter

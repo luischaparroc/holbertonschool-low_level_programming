15ad3544167b35e430063dae153380fdc4c2baed
luischaparoc <lfchaparro@outlook.com>
2019-11-15 22:01:47 +0000

    [Fix]: calculates ncolumns correctly Modified: 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,18 +7,20 @@ def island_perimeter(grid):
     perimeter = 0
 
     nrows = len(grid)
-    ncolumns = len(grid)
+
+    if grid != []:
+        ncolumns = len(grid[0])
 
     for a in range(nrows):
         for b in range(ncolumns):
             if grid[a][b] == 1:
-                if (a - 1) != 1 and grid[a - 1][b] == 0:
+                if (a - 1) == 1 or grid[a - 1][b] == 0:
                     perimeter += 1
-                if (a + 1) != nrows and grid[a + 1][b] == 0:
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
                     perimeter += 1
-                if (b - 1) != -1 and grid[a][b - 1] == 0:
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
                     perimeter += 1
-                if (b + 1) != ncolumns and grid[a][b + 1] == 0:
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
                     perimeter += 1
 
     return perimeter

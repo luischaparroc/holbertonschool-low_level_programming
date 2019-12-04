e74c22ea49abde167c2a99be9f2528b82de3230b
luischaparoc <lfchaparro@outlook.com>
2019-12-04 20:35:36 +0000

    Fixing task 14

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,17 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * recursive_balance - measures the balance factor of a binary tree
+ * recursive_height - measures the height of a binary tree
  *
  * @tree: tree root
- * Return: balance factor
+ * Return: height
  */
-int recursive_balance(const binary_tree_t *tree)
+size_t recursive_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left = recursive_height(tree->left);
+	right = recursive_height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
+/**
+ * binary_tree_height - calls recursive_height to return the height
+ * of a binary tree
+ *
+ * @tree: tree root
+ * Return: height of the tree or 0 if tree is NULL;
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (-1);
 
-	return (recursive_balance(tree->left) - recursive_balance(tree->right));
+	return (recursive_height(tree) - 1);
 }
 
 /**
@@ -26,5 +50,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (recursive_balance(tree));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }

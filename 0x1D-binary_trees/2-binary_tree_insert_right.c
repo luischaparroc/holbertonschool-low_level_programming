e0b4be131288c0a174679821bdd9afc83f3f7d12
luischaparoc <lfchaparro@outlook.com>
2019-12-04 17:09:34 +0000

    Fixing task 1 & 2

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);

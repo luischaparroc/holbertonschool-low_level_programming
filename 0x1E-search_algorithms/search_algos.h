37841d3beffaea95470e500c3804346438ab3d6f
luischaparroc <lfchaparro@outlook.com>
2020-01-14 21:53:54 +0000

    [Feature]: adding Advanced binary search New file: 104-advanced_binary.c Modified: search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif

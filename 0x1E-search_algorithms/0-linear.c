75b8f1e7669918d40619d802b2cfb57ec763652a
luischaparroc <lfchaparro@outlook.com>
2020-01-14 14:42:47 +0000

    [Update]: adding NULL case Modified: 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i;
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);

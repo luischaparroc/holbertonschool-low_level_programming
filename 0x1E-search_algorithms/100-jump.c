4d20e35f9851e2349cac7afd0949cbb0b1a716b2
luischaparroc <lfchaparro@outlook.com>
2020-01-14 19:54:19 +0000

    [Update]: change first loop

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,22 +21,19 @@ int jump_search(int *array, size_t size, int value)
 	k = 0;
 	prev = index = 0;
 
-	while (index < (int)size && array[index] < value)
-	{
+	do {
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-		if (index >= (int)size)
-			break;
+
 		if (array[index] == value)
 			return (index);
 		k++;
 		prev = index;
 		index = k * m;
-	}
+	} while (index < (int)size && array[index] < value);
 
-	if (index)
-		printf("Value found between indexes [%d] and [%d]\n", prev, index);
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
 
-	for (; index && prev <= index && prev < (int)size; prev++)
+	for (; prev <= index && prev < (int)size; prev++)
 	{
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)

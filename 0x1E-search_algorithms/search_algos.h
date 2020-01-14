5a1b114d9ee4a40341a79ff8ac693fe1cb146a93
luischaparroc <lfchaparro@outlook.com>
2020-01-14 14:31:14 +0000

    [Feature]: adding function that executes linear search algorithm New file: 0-linear.c Modified: search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef _SORTING_ALGOS_H_
 #define _SORTING_ALGOS_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
